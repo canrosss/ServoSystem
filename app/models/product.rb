520f4328c2cb4cefc95f8667a8f35717878cda55
Canros <canros.mx@gmail.com>
2017-12-04 03:50:10 -0600

    Se añade tabla para elementos.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   validates :name, presence: :true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: :true, length: {minimum: 3, maximum: 400}
+  validates :description, presence: :true, length: {minimum: 3}
 end
