e65eea46c7a5b14dc5d91b10da41a9a2bbe3128d
Canros <canros.mx@gmail.com>
2017-12-04 01:31:47 -0600

    Se arreglan los links que estaban faltando en el nav.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   #Cuando el navegador pida /pages/home manda a controller#vista home o index
   #   ruta -> controller#vista
   get '/contact', to:'pages#contact'
+  get '/about', to:'pages#about'
   root :to => 'pages#home'
 
 
