91f20a3d368af30c81f32dd53e980b6e6931674c
Canros <canros.mx@gmail.com>
2017-12-07 03:27:24 -0600

    Se añaden algunos productos de prueba... falta afinar colores, fuente quizas posiciones y tamaños.

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,12 @@ module ServoSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/images/products/bedmill/dpm2"
 
+    config.assets.paths << "#{Rails.root}/app/assets/images/products/bedmill"
+
+    config.assets.paths << "#{Rails.root}/app/assets/images/products/bedmill"
+
+    config.assets.paths << "#{Rails.root}/app/assets/images/products/lathe"
     #Añadiendo etiquetas
 
 
