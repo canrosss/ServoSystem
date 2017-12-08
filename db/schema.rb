1f390eddb8210219319c2c91fe4b510495095821
Canros <canros.mx@gmail.com>
2017-12-08 04:20:48 -0600

    Se crea la tabla users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205074537) do
+ActiveRecord::Schema.define(version: 20171208101846) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20171205074537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "body"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
